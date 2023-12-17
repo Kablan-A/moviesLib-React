3bc57182e08255a6f183187eba09d7d3ccf0edb2
Kablan Abaidullayev <abaidullayev.k@gmail.com>
2023-12-17 22:53:39 +0600

    updated api

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://77c8-2-73-167-114.ngrok-free.app",
-  headers: { "ngrok-skip-browser-warning": "true" },
+  baseURL: "https://movies-lib-trcx.onrender.com",
 });
