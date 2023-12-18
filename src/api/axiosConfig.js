86d078221eea52b988fc80379fcd1a99a509ffac
Kablan Abaidullayev <abaidullayev.k@gmail.com>
2023-12-18 17:23:34 +0600

    last try please

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,9 +2,4 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://movies-lib-trcx.onrender.com",
-  headers: {
-    "Access-Control-Allow-Origin": "https://movies-lib-trcx.onrender.com",
-    "Access-Control-Allow-Methods": "GET, POST, PUT",
-    "Access-Control-Allow-Headers": "Content-Type",
-  },
 });
