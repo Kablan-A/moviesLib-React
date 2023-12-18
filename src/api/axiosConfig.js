72e40eee613d4f6f8126a9a4a4b963e1323a59c4
Kablan Abaidullayev <abaidullayev.k@gmail.com>
2023-12-18 17:39:04 +0600

    I should read more about it

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,4 +2,9 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://movies-lib-trcx.onrender.com",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT",
+    "Access-Control-Allow-Headers": "Content-Type",
+  },
 });
